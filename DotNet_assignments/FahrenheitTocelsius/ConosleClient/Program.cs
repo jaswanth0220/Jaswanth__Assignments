f50bf0d6aba0e976515315a4d8395d73817f6d91
Jaswanth <67514357+jaswanth187@users.noreply.github.com>
2024-07-24 15:03:34 +0530

    Library fahrenheit to celsius

---
+++ DotNet_assignments/FahrenheitTocelsius/ConosleClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using FahrenheitTocelsius;
+
+namespace ConosleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature t = new Temperature();
+            Console.WriteLine(t.FTC(10));
+            Console.WriteLine(t.CTF(10));
+        }
+    }
+}
---
+++ DotNet_assignments/FahrenheitTocelsius/FahrenheitTocelsius/Temperature.cs
@@ -0,0 +1,17 @@
+﻿namespace FahrenheitTocelsius
+{
+    public class Temperature
+    {
+        public double CTF(double temp)
+        {
+           double F =   (9/5*temp)+32;
+            return F;
+        }
+
+        public double FTC(double temp)
+        {
+            double C = (temp - 32) * 5 / 9;
+            return C;
+        }
+    }
+}
